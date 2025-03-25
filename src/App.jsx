ee8558e9988cbb3297ca911e9909d3f49094de51
MarsalShyam <shyam12marsal@gmail.com>
2025-03-25 23:34:06 +0530

    deployment resolve

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar'
 // import Manager from './components/Manager'
 import Footer from './components/Footer'
-import NoteManager from './components/noteManager';
+import NoteManager from './components/NoteManager';
 
 function App() {
   
